c6f7b707b9ffcf579c626709462cf087d47f29a0
Madelyn Vagle <madelyn.vagle@gmail.com>
2022-08-02 21:53:52 -0500

    fix quotes

--- data/wyoming.js
+++ data/wyoming.js
@@ -5,16 +5,16 @@ var wyoming =
             banner: "/vacationlands/wyoming2_incrpl.png"
         },
         discover: {
-            paragraph1: 'The &ldquo;Old West&rdquo; described in this brochure draws on the myth of the frontier to entice travelers.',
-            paragraph2: "Frederick Jackson Turner&apos;s Frontier Thesis popularized this romanticized version of Western history, and Western movies, televised Westerns, and Western novels cemented its place in the national imagination."
+            paragraph1: 'The “Old West” described in this brochure draws on the myth of the frontier to entice travelers.',
+            paragraph2: "Frederick Jackson Turner's Frontier Thesis popularized this romanticized version of Western history, and Western movies, televised Westerns, and Western novels cemented its place in the national imagination."
         },
         stay: {
             paragraph1: "The 1946 Western Vacationlands brochure advertises two Union Pacific lodges in Grand Teton National Park; Jackson Lake Lodge and Teton Lodge.",
-            paragraph2: 'Both lodges offered planned excursion opportunities as well as horse and equipment rental for individual use. The brochure advertises that the lodges and cabins are &ldquo;built of native log, rustic in atmosphere,&rdquo; but assures prospective visitors that they are &ldquo;thoroughly comfortable.&rdquo;',
+            paragraph2: 'Both lodges offered planned excursion opportunities as well as horse and equipment rental for individual use. The brochure advertises that the lodges and cabins are “built of native log, rustic in atmosphere,” but assures prospective visitors that they are “thoroughly comfortable.”',
         }, 
         play: {
-            paragraph1: 'The brochure mentions a variety of outdoor attractions within Grand Teton National Park. &ldquo;Saddle horses, motor and row boats&rdquo; are all on offer from the park lodges as ways to explore the area&apos;s natural beauty.',
-            paragraph2: 'Theodore Roosevelt believed the frontier cultivated a &ldquo;vigorous manliness.&rdquo; The brochure echoes these ideas by connecting Grand Teton to the collective imagination of &ldquo;every American boy.&rdquo;'
+            paragraph1: "The brochure mentions a variety of outdoor attractions within Grand Teton National Park. “Saddle horses, motor and row boats” are all on offer from the park lodges as ways to explore the area's natural beauty.",
+            paragraph2: 'Theodore Roosevelt believed the frontier cultivated a “vigorous manliness.” The brochure echoes these ideas by connecting Grand Teton to the collective imagination of “every American boy.”'
         }
 
     }
